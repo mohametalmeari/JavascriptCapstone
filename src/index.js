3e0e4f66e251cf0ba144427b4f0d0c0035c134fe
mohametalmeari <mohamet.almeari@gmail.com>
2023-05-28 01:38:10 +0300

    Style for different screen sizes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
+<<<<<<< Updated upstream
 import './style.css';
+=======
+import './style.css';
+import icon from './images/pokeball-icon.png';
+import createCards from './modules/cards.js';
+import fetchNamesApi from './modules/namesApi.js';
+
+const BallIcon = document.getElementById('icon');
+BallIcon.src = icon;
+const NumOfCards = 32;
+
+createCards(0, NumOfCards, icon);
+fetchNamesApi(NumOfCards);
+
+const XMenu = document.querySelector('.x-menu');
+const Menu = document.querySelector('.menu');
+const menuIcon = document.querySelector('.menu-icon')
+XMenu.addEventListener('click', () =>{
+    Menu.style.display='none';
+    document.body.style.overflowY = 'scroll';
+});
+
+menuIcon.addEventListener('click', () => {
+    Menu.style.display='flex';
+    document.body.style.overflowY = 'hidden';
+})
+>>>>>>> Stashed changes
